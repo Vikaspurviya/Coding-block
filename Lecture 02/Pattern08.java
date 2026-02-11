5d5057dc1bc14b6a0e4f2e901597ec4cd8287b4f
Vikaspurviya <vikasthakur9303@gmail.com>
2026-02-11 10:38:35 +0530

    Update

--- Lecture 02/Pattern08.java
+++ Lecture 02/Pattern08.java
@@ -1,9 +1,9 @@
 public class Pattern08 {
    public static void main(String[] args) {
     int n=5;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(j==i || j==n-i+1){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==j || i+j==n+1){
                 System.out.print("* ");
             }
             else{ 
