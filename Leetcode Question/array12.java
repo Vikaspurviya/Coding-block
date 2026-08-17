edcf293f6bd0de8a99052d9d76f8190b1c0d4065
Vikaspurviya <vikasthakur9303@gmail.com>
2026-08-17 14:41:39 +0530

    1

---
+++ Leetcode Question/array12.java
@@ -0,0 +1,32 @@
+public class array12 {
+    public static void main(String[] args) {
+        int[]arr={1,2,3,4};
+        int carry=1;
+        arr= add(arr,carry);
+
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+    }
+    public static int[] add(int[]arr,int carry){
+        for(int i=arr.length-1;i>=0;i--){
+         if(arr[i]+carry<=9){
+            arr[i]+=carry;
+            carry=0;
+            break;
+         }
+         else{
+            arr[i]=0;
+            carry=1;
+         }
+        }
+        if(carry==1){
+            int[] result=new int[arr.length + 1];
+                result[0]=1;
+                return result ;
+            }
+             return arr;
+        }
+   
+    }
+
