94750db7dd3cc6f1ce955e9c1620c8b9c74bbe27
Vikaspurviya <vikasthakur9303@gmail.com>
2026-09-12 09:27:48 +0530

    3

---
+++ Leetcode Question/insertion.java
@@ -0,0 +1,20 @@
+public class insertion {
+    public static void print(int[]arr){
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[]arr={3,2,5,16,7,1,10};
+        for(int i=0;i<arr.length;i++){
+            int j=i;
+            while(j>0 && arr[j]<arr[j-1]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
+        print(arr);
+    }
+}
